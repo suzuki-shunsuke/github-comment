c9c528cbf064dbe14d3192308eebbb560550a353
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-07-23 17:35:50 +0900

    test: add a test

--- pkg/api/post_internal_test.go
+++ pkg/api/post_internal_test.go
@@ -1,11 +1,62 @@
 package api
 
 import (
+	"context"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/suzuki-shunsuke/github-comment/pkg/comment"
+	"github.com/suzuki-shunsuke/github-comment/pkg/option"
+	"github.com/suzuki-shunsuke/github-comment/pkg/template"
 )
+
+func TestPostController_getCommentParams(t *testing.T) {
+	data := []struct {
+		title string
+		ctrl  PostController
+		exp   comment.Comment
+		isErr bool
+		opts  option.PostOptions
+	}{
+		{
+			title: "if there is a standard input, treat it as the template",
+			ctrl: PostController{
+				HasStdin: func() bool {
+					return true
+				},
+				Stdin: strings.NewReader("hello"),
+				Getenv: func(k string) string {
+					return ""
+				},
+				Renderer: template.Renderer{},
+			},
+			opts: option.PostOptions{
+				Org:      "suzuki-shunsuke",
+				Repo:     "github-comment",
+				Token:    "xxx",
+				PRNumber: 1,
+			},
+			exp: comment.Comment{
+				Org:      "suzuki-shunsuke",
+				Repo:     "github-comment",
+				PRNumber: 1,
+				Body:     "hello",
+			},
+		},
+	}
+	ctx := context.Background()
+	for _, d := range data {
+		d := d
+		cmt, err := d.ctrl.getCommentParams(ctx, d.opts)
+		if d.isErr {
+			require.NotNil(t, err)
+			return
+		}
+		require.Nil(t, err)
+		require.Equal(t, d.exp, cmt)
+	}
+}
 
 func TestPostController_readTemplateFromStdin(t *testing.T) {
 	data := []struct {
