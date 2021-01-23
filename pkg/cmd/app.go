74c77f60abc9318e542f62be122824ad48f736f0
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2021-01-23 18:32:08 +0900

    refactor: refactor cmd

--- pkg/cmd/app.go
+++ pkg/cmd/app.go
@@ -14,18 +14,143 @@ type Runner struct {
 	Stderr io.Writer
 }
 
-func (runner Runner) Run(ctx context.Context, args []string) error {
-	postCommand := runner.postCommand()
-	execCommand := runner.execCommand()
-	initCommand := runner.initCommand()
+func (runner *Runner) Run(ctx context.Context, args []string) error { //nolint:funlen
 	app := cli.App{
 		Name:    "github-comment",
 		Usage:   "post a comment to GitHub",
 		Version: constant.Version,
 		Commands: []*cli.Command{
-			&postCommand,
-			&execCommand,
-			&initCommand,
+			{
+				Name:   "post",
+				Usage:  "post a comment",
+				Action: runner.postAction,
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "org",
+						Usage: "GitHub organization name",
+					},
+					&cli.StringFlag{
+						Name:  "repo",
+						Usage: "GitHub repository name",
+					},
+					&cli.StringFlag{
+						Name:    "token",
+						Usage:   "GitHub API token",
+						EnvVars: []string{"GITHUB_TOKEN", "GITHUB_ACCESS_TOKEN"},
+					},
+					&cli.StringFlag{
+						Name:  "sha1",
+						Usage: "commit sha1",
+					},
+					&cli.StringFlag{
+						Name:  "template",
+						Usage: "comment template",
+					},
+					&cli.StringFlag{
+						Name:    "template-key",
+						Aliases: []string{"k"},
+						Usage:   "comment template key",
+						Value:   "default",
+					},
+					&cli.StringFlag{
+						Name:  "config",
+						Usage: "configuration file path",
+					},
+					&cli.IntFlag{
+						Name:  "pr",
+						Usage: "GitHub pull request number",
+					},
+					&cli.StringSliceFlag{
+						Name:  "var",
+						Usage: "template variable",
+					},
+					&cli.BoolFlag{
+						Name:  "dry-run",
+						Usage: "output a comment to standard error output instead of posting to GitHub",
+					},
+					&cli.BoolFlag{
+						Name:    "skip-no-token",
+						Aliases: []string{"n"},
+						Usage:   "works like dry-run if the GitHub Access Token isn't set",
+						EnvVars: []string{"GITHUB_COMMENT_SKIP_NO_TOKEN"},
+					},
+					&cli.BoolFlag{
+						Name:    "silent",
+						Aliases: []string{"s"},
+						Usage:   "suppress the output of dry-run and skip-no-token",
+					},
+					&cli.BoolFlag{
+						Name:  "stdin-template",
+						Usage: "read standard input as the template",
+					},
+				},
+			},
+			{
+				Name:   "exec",
+				Usage:  "execute a command and post the result as a comment",
+				Action: runner.execAction,
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "org",
+						Usage: "GitHub organization name",
+					},
+					&cli.StringFlag{
+						Name:  "repo",
+						Usage: "GitHub repository name",
+					},
+					&cli.StringFlag{
+						Name:    "token",
+						Usage:   "GitHub API token",
+						EnvVars: []string{"GITHUB_TOKEN", "GITHUB_ACCESS_TOKEN"},
+					},
+					&cli.StringFlag{
+						Name:  "sha1",
+						Usage: "commit sha1",
+					},
+					&cli.StringFlag{
+						Name:  "template",
+						Usage: "comment template",
+					},
+					&cli.StringFlag{
+						Name:    "template-key",
+						Aliases: []string{"k"},
+						Usage:   "comment template key",
+						Value:   "default",
+					},
+					&cli.StringFlag{
+						Name:  "config",
+						Usage: "configuration file path",
+					},
+					&cli.IntFlag{
+						Name:  "pr",
+						Usage: "GitHub pull request number",
+					},
+					&cli.StringSliceFlag{
+						Name:  "var",
+						Usage: "template variable",
+					},
+					&cli.BoolFlag{
+						Name:  "dry-run",
+						Usage: "output a comment to standard error output instead of posting to GitHub",
+					},
+					&cli.BoolFlag{
+						Name:    "skip-no-token",
+						Aliases: []string{"n"},
+						Usage:   "works like dry-run if the GitHub Access Token isn't set",
+						EnvVars: []string{"GITHUB_COMMENT_SKIP_NO_TOKEN"},
+					},
+					&cli.BoolFlag{
+						Name:    "silent",
+						Aliases: []string{"s"},
+						Usage:   "suppress the output of dry-run and skip-no-token",
+					},
+				},
+			},
+			{
+				Name:   "init",
+				Usage:  "scaffold a configuration file if it doesn't exist",
+				Action: runner.initAction,
+			},
 		},
 		Flags: []cli.Flag{
 			&cli.StringFlag{
--- pkg/cmd/init.go
+++ pkg/cmd/init.go
@@ -6,14 +6,6 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-func (runner Runner) initCommand() cli.Command {
-	return cli.Command{
-		Name:   "init",
-		Usage:  "scaffold a configuration file if it doesn't exist",
-		Action: runner.initAction,
-	}
-}
-
 // initAction is an entrypoint of the subcommand "init".
 func (runner Runner) initAction(c *cli.Context) error {
 	ctrl := api.InitController{
