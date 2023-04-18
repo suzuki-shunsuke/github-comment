ba353e40f4e1bcded5829b3719c2d00361a42f11
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-04-18 12:35:47 +0900

    feat: rename environment variables and support passing variables via environment variables

--- pkg/cmd/hide.go
+++ pkg/cmd/hide.go
@@ -29,16 +29,9 @@ func parseHideOptions(opts *option.HideOptions, c *cli.Context) error {
 	opts.Condition = c.String("condition")
 	opts.SHA1 = c.String("sha1")
 
-	vars, err := parseVarsFlag(c.StringSlice("var"))
+	vars, err := parseVars(c)
 	if err != nil {
 		return err
-	}
-	varFiles, err := parseVarFilesFlag(c.StringSlice("var-file"))
-	if err != nil {
-		return err
-	}
-	for k, v := range varFiles {
-		vars[k] = v
 	}
 	opts.Vars = vars
 
