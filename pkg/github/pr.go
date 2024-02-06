1e45d927a974134ab52f11822d72e6b5932a9ffe
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-02-06 17:25:49 +0900

    chore: update go-github to v58

--- pkg/github/pr.go
+++ pkg/github/pr.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/google/go-github/v57/github"
+	"github.com/google/go-github/v58/github"
 )
 
 func (c *Client) PRNumberWithSHA(ctx context.Context, owner, repo, sha string) (int, error) {
